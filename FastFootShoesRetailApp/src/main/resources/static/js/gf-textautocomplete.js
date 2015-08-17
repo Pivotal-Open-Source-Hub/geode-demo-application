71251882cf4aa862ea290d850ea3a4649397819c
Rebecca <rputinski@pivotal.io>
2015-08-17 09:59:37 -0400

    UI Updates & fixes

--- FastFootShoesRetailApp/src/main/resources/static/js/gf-textautocomplete.js
+++ FastFootShoesRetailApp/src/main/resources/static/js/gf-textautocomplete.js
@@ -1,82 +1,76 @@
-var baseUrl = 'http://127.0.0.1:8080/';
 $(function() {
-	$("#productsAutocomplete").autocomplete(
-			{
-				minLength : 3,
-				select : function(event, ui) {
-					document.getElementById("productId").value = ui.item.id;
-				},
-				source : function(request, response) {
-					$.get(baseUrl + 'products' + '?type=' + request.term,
-							function(products) {
-								response(jQuery.map(products, function(product,
-										indx) {
-									var label = ' Brand: '
-											+ product.brand
-											+ ' Type: '
-											+ product.type
-											+ ' Color: '
-											+ product.color
-											+ '  Size: '
-											+ product.size
-											+ (product.gender == 'M' ? ' Mens'
-													: ' Womens');
-									return {
-										id : product.id,
-										value : label,
-										label : label
-									};
-								}));
-							});
-				}
-			});
+	$("#productsAutocomplete").autocomplete({
+		minLength: 3,
+		select: function(event, ui) {
+			document.getElementById("productId").value = ui.item.id;
+		},
+		source: function(request, response) {
+			$.get(window.location.origin + '/products' + '?type=' + request.term,
+				function(products) {
+					response(jQuery.map(products, function(product,
+							indx) {
+						var label = ' Brand: '
+								+ product.brand
+								+ ' Type: '
+								+ product.type
+								+ ' Color: '
+								+ product.color
+								+ '  Size: '
+								+ product.size
+								+ (product.gender == 'M' ? ' Mens'
+										: ' Womens');
+						return {
+							id : product.id,
+							value : label,
+							label : label
+						};
+					}));
+				});
+		}
+	});
 
 	// customer
-	$("#customerAutocomplete").autocomplete(
-			{
-				minLength : 3,
-				select : function(event, ui) {
-					document.getElementById("customerId").value = ui.item.id;
-				},
-				source : function(request, response) {
-					$.get(baseUrl + 'customers' + '?email=' + request.term,
-							function(customers) {
-								response(jQuery.map(customers, function(
-										customer, indx) {
-									var label = customer.name + ' '
-											+ customer.emailAddress;
-									return {
-										id : customer.id,
-										value : label,
-										label : label
-									};
-								}));
-							});
-				}
-			});
+	$("#customerAutocomplete").autocomplete({
+		minLength: 3,
+		select: function(event, ui) {
+			document.getElementById("customerId").value = ui.item.id;
+		},
+		source: function(request, response) {
+			$.get(window.location.origin + '/customers' + '?email=' + request.term,
+					function(customers) {
+						response(jQuery.map(customers, function(
+								customer, indx) {
+							var label = customer.name + ' '
+									+ customer.emailAddress;
+							return {
+								id : customer.id,
+								value : label,
+								label : label
+							};
+						}));
+					});
+		}
+	});
 
 	// customer search (in menu bar)
-	$("#customerSearchAutocomplete")
-			.autocomplete(
-					{
-						minLength : 3,
-						select : function(event, ui) {
-							document.getElementById("searchCustomerId").value = ui.item.id;
-						},
-						source : function(request, response) {
-							$.get(baseUrl + 'customers' + '?email='
-									+ request.term, function(customers) {
-								response(jQuery.map(customers, function(
-										customer, indx) {
-									var label = customer.emailAddress;
-									return {
-										id : customer.id,
-										value : label,
-										label : label
-									};
-								}));
-							});
-						}
-					});
-
+	$("#customerSearchAutocomplete").autocomplete({
+		minLength: 3,
+		select: function(event, ui) {
+			document.getElementById("searchCustomerId").value = ui.item.id;
+		},
+		source: function(request, response) {
+			$.get(window.location.origin + '/customers' + '?email='
+					+ request.term, function(customers) {
+				response(jQuery.map(customers, function(
+						customer, indx) {
+					var label = customer.emailAddress;
+					return {
+						id : customer.id,
+						value : label,
+						label : label
+					};
+				}));
+			});
+		}
+	});
 });
