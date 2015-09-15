f66170fa8790e225729cf88413db928f6502c43e
lshannonPivotal <lshannon@pivotal.io>
2015-09-14 23:59:49 -0400

    Update to demo clean up
    Added deletion of the working folders to the stop demo script

--- demo/single-machine-mode/stopDemo.sh
+++ demo/single-machine-mode/stopDemo.sh
@@ -12,6 +12,11 @@ echo "Deleting the Derby DB files"
 rm -fr fastfootshoes
 
 echo "Cleaning Up Working Folders"
-
+rm -fr ../geode-server-package/locatorA
+rm -fr ../geode-server-package/locatorB
+rm -fr ../geode-server-package/serverA
+rm -fr ../geode-server-package/serverB
+rm -fr ../geode-server-package/serverC
+rm -fr ../geode-server-package/serverD
 
 echo "Shut Down & Clean Up Completed"
